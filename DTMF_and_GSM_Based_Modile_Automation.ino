22061712e5e566d0817b01204ee1c7105b59d667
jth-1996 <jithin.1005@gmail.com>
2017-12-10 10:18:14 +0530

    Update DTMF_and_GSM_Based_Modile_Automation.ino

--- DTMF_and_GSM_Based_Modile_Automation.ino
+++ DTMF_and_GSM_Based_Modile_Automation.ino
@@ -2,19 +2,17 @@
  DTMF (mobile) based Home automation
 
  Author : Jithin T; DEBUT LABS (jithin.1005@gmail.com)
- Authored on : 01 March 2017
+ Authored on : 10 December 2017
 
- This Arduino Sketch is part of DTMF and GSM Based Home Automation tutorial.
+ This Arduino Sketch is part of DTMF Controlled Robo Car tutorial.
 
  this sketch will do all what is necessary to answer an incomming call (from any number), receive keypad number dials (from remote caller) and operate relays as per instruction.
- Here we have intrerfaced only two relays. First keytone will deceive the relay ('1' for Relay 1 -connected to D8- and '2' for Relay 2 -connected to D9-)
- Second key tone will decide the status of the Relay ('1' for relay 'ON' and '2' for Relay 'OFF') any other key strokes will be neglected.
+ Here we have intrerfaced two motors. Key press for different actions have been mapped. You can change them accordingly.
 
  Circuit Necessary
  > Arduino Uno
  > GSM module - with SIM800
- >Relays
-
+ > L293D module
  
 **************************************************************/
 
@@ -25,26 +23,41 @@
 # define GSM_TX 11 // Connect RX of GSM module
 
 
-// Defining interfacing pins for Relay board
-# define RELAY_1 8
-# define RELAY_2 9
+// Defining interfacing pins for Motor Control (ie pins to L293D)
+// Code is designed such that the two motor terminals are named A & B. and is assumed that the motor will rotate clock wise when A terminal is HIGH and B is LOW
+# define LEFT_A 8 // Connect the A terminal of LEFT motor to Digital Pin 8 of Arduino
+# define LEFT_B 9
+# define RIGHT_A 10
+# define RIGHT_B 11
+
+// Defining the key press to drive the car.
+# define FORWARD '2'
+# define RIGHT '6'
+# define LEFT '4'
+# define BACKWARD 8
+# define STOP 5
+
 
 SoftwareSerial gsm_board(GSM_RX,GSM_TX);
 
-boolean call_flag=0, relay_flag=0;
+boolean call_flag=0;
 int i=0,j=0,x=-1;
-char n[3];
+char n;
 void gsm_initilaize();// used to inilitize the gsm and chk all its parameters
-void relay(); // used to control relay outputs.
+void move(); // used to control motor outputs.
 
 void setup() {
   // put your setup code here, to run once:
   gsm_board.begin(9600);
   Serial.begin(9600);
-  pinMode(RELAY_1,OUTPUT);
-  pinMode(RELAY_2,OUTPUT);
-  digitalWrite(RELAY_1,LOW);
-  digitalWrite(RELAY_2,LOW);
+  pinMode(LEFT_A,OUTPUT);
+  pinMode(LEFT_B,OUTPUT);
+  pinMode(RIGHT_A,OUTPUT);
+  pinMode(RIGHT_B,OUTPUT);
+  digitalWrite(LEFT_A,LOW);
+  digitalWrite(LEFT_B,LOW);
+  digitalWrite(RIGHT_A,LOW);
+  digitalWrite(RIGHT_B,LOW);
   gsm_initilaize();
 
 }
@@ -67,7 +80,7 @@ void loop() {
     x=gsm_data.indexOf("RING");
     if(x>-1)
     {
-      delay(5000);
+      delay(5000); // change this delay value to change the delay in attending the call by your car.
       gsm_board.println("ATA");
       call_flag=1;
     }
@@ -77,23 +90,16 @@ void loop() {
     x=gsm_data.indexOf("DTMF"); //checkinh dtmf and storing approprietly
     if(x>-1)
     {
-      n[j]=gsm_data[x+6];
-      Serial.println(n[j]);
-      j++;
+      n=gsm_data[x+6];
+      Serial.println(n);
+      move(n); // call motor control function to initiate the change demanded by controller
     }
     x=gsm_data.indexOf("NO CARRIER"); // Checking whether call is still connected or not
     if(x>-1)
     {
       gsm_board.println("ATH");
-      relay_flag=1;
       call_flag=0;
-      j=0;
     }
-  }
-
-  if(relay_flag) // If a call was resently disconnected, changing relay states accordingly
-  {
-    relay();
   }
   
 }
@@ -156,35 +162,13 @@ void gsm_initilaize()
 
 /////////////////////relay begins///////////////////////////
 
-void relay()
+void move()
 {
-  if(n[0]=='1') // if RELAY_1  was selected
-    {
-      if(n[1]=='1')
-      {
-        digitalWrite(RELAY_1,HIGH); //relay 1 on
-        Serial.println("RELAY 1 ON");
-      }
-      else if(n[1]=='2')
-      {
-        digitalWrite(RELAY_1,LOW);// relay 1 off
-        Serial.println("RELAY 1 OFF");
-      }
-    }
-    else if(n[0]=='2') // if RELAY_2  was selected
-    {
-      if(n[1]=='1')
-      {
-        digitalWrite(RELAY_2,HIGH); // realy 2 on
-        Serial.println("RELAY 2 ON");
-      }
-      else if(n[1]=='2')
-      {
-        digitalWrite(RELAY_2,LOW); // relay 2 off
-        Serial.println("RELAY 2 OFF");
-      }
-    }
-    relay_flag=0;
+  switch(n)
+  {
+   case "2" :
+    forw
+  }
 }
 
 /////////////////////relay ends///////////////////////////
